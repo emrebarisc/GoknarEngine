0373122c1966d0faed2e6aca2ecdec8c1dd62aa6
Baris <emrebarisc@gmail.com>
2020-01-18 21:25:47 +0300

    Input key map is added

--- GameProject/src/Airplane.cpp
+++ GameProject/src/Airplane.cpp
@@ -28,7 +28,9 @@ Airplane::Airplane() : ObjectBase()
 
 	SetTickable(true);
 
-	engine->GetInputManager()->AddKeyboardInputDelegate(32, INPUT_ACTION::G_PRESS, std::bind(&Airplane::SpaceKeyDown, this));
+	engine->GetInputManager()->AddKeyboardInputDelegate(KEY_MAP::SPACE, INPUT_ACTION::G_PRESS, std::bind(&Airplane::SpaceKeyDown, this));
+	engine->GetInputManager()->AddKeyboardInputDelegate(KEY_MAP::A, INPUT_ACTION::G_PRESS, std::bind(&Airplane::GoLeft, this));
+	engine->GetInputManager()->AddKeyboardInputDelegate(KEY_MAP::D, INPUT_ACTION::G_PRESS, std::bind(&Airplane::GoRight, this));
 }
 
 Airplane::~Airplane()
@@ -48,18 +50,13 @@ void Airplane::Tick(float deltaTime)
 	static float elapsedTime = 0.f;
 	elapsedTime += deltaTime;
 
-	float airplaneRotation = DEGREE_TO_RADIAN(0.174f * cos(elapsedTime));
-	planeMeshComponent_->SetRelativePosition(planeMeshComponent_->GetRelativePosition() + Vector3(0.0125f * cos(elapsedTime), 0.f, 0.f));
-	planeMeshComponent_->SetRelativeRotation(planeMeshComponent_->GetRelativeRotation() + Vector3(0.f, airplaneRotation, 0.f));
-
-	propellerMeshComponent_->SetRelativePosition(propellerMeshComponent_->GetRelativePosition() + Vector3(0.0125f * cos(elapsedTime), 0.f, 0.f));
-	propellerMeshComponent_->SetRelativeRotation(propellerMeshComponent_->GetRelativeRotation() + Vector3(0.f, airplaneRotation + DEGREE_TO_RADIAN(deltaTime * propellerRotationSpeed_), 0.f));
+	propellerMeshComponent_->SetRelativeRotation(propellerMeshComponent_->GetRelativeRotation() + Vector3(0.f, DEGREE_TO_RADIAN(deltaTime * propellerRotationSpeed_), 0.f));
 
 	SetWorldPosition(GetWorldPosition() + Vector3(0.f, 5.f * deltaTime, 0.f));
 
 	Camera* mainCamera = engine->GetCameraManager()->GetActiveCamera();
 	Vector3 cameraPosition = mainCamera->GetPosition();
-	mainCamera->SetPosition(Vector3(cameraPosition.x, GetWorldPosition().y, cameraPosition.z));
+	mainCamera->SetPosition(GetWorldPosition() + Vector3(0.f, 0.f, 10.f));
 }
 
 void Airplane::SpaceKeyDown()
