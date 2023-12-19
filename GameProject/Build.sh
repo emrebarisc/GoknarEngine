0d43130882276a2484b9e13dcf560fc05efd417b
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2023-12-19 23:26:30 +0300

    Add shell script for building

---
+++ GameProject/Build.sh
@@ -0,0 +1,48 @@
+isDebugBuild=false
+cleanBuild=false
+runAfterBuild=false
+directoryName=""
+gameName="GameProject"
+
+for argument in "$@"
+do
+    if [[ "$argument" == "debug" ]]; then
+    	isDebugBuild=true
+    else
+	    if [[ "$argument" == "run" ]]; then
+	    	runAfterBuild=true
+	    else
+		    if [[ "$argument" == "clean" ]]; then
+		    	cleanBuild=true
+		    fi
+	    fi
+    fi
+done
+
+if [ "$isDebugBuild" = true ]; then
+	directoryName="Build_Debug"
+	
+	if [ "$cleanBuild" = true ]; then
+		rm -rf $directoryName && mkdir $directoryName && cd $directoryName && cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j 8
+	else
+		if ! [ -d $directoryName ]; then
+			mkdir $directoryName
+		fi
+		cd $directoryName && cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j 8
+	fi
+else
+	directoryName="Build_Release"
+	if [ "$cleanBuild" = true ]; then
+		rm -rf $directoryName && mkdir $directoryName && cd $directoryName && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j 8
+	else
+		if ! [ -d $directoryName ]; then
+			mkdir $directoryName
+		fi
+		cd $directoryName && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j 8
+	fi
+fi
+
+
+if [ "$runAfterBuild" = true ]; then
+	./$gameName
+fi
