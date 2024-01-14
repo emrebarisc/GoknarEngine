b2833519d1c61b291e86cc0e648b87fcf921fc7f
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2024-01-14 21:27:38 +0300

    GameProject: Make main render type deferred

--- GameProject/Source/Game.cpp
+++ GameProject/Source/Game.cpp
@@ -43,7 +43,7 @@ Game::Game() : Application()
 {
 	engine->SetApplication(this);
 
-	engine->GetRenderer()->SetMainRenderType(RenderPassType::Forward);
+	engine->GetRenderer()->SetMainRenderType(RenderPassType::Deferred);
 
 	std::chrono::steady_clock::time_point lastFrameTimePoint = std::chrono::steady_clock::now();
 	mainScene_->ReadSceneData("Scenes/Scene_2.xml");
