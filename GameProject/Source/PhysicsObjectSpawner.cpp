87ba1ed2e78c5d29699e33bc87a51546e2d35743
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2024-01-27 23:28:26 +0300

    GameProject: Add and spawn objects with sphere collision component

--- GameProject/Source/PhysicsObjectSpawner.cpp
+++ GameProject/Source/PhysicsObjectSpawner.cpp
@@ -2,13 +2,18 @@
 
 #include <random>
 
+#include "Archer.h"
 #include "Objects/PhysicsBox.h"
+#include "Objects/PhysicsSphere.h"
 
 #include "Engine.h"
+#include "Game.h"
 #include "Managers/InputManager.h"
 
 PhysicsObjectSpawner::PhysicsObjectSpawner()
 {
+    SetIsTickable(true);
+
     groundPhysicsBox_ = new PhysicsBox();
     groundPhysicsBox_->SetWorldPosition(initialPosition_);
     groundPhysicsBox_->SetWorldScaling(Vector3{ 10.f, 10.f, 0.25f });
@@ -21,7 +26,21 @@ PhysicsObjectSpawner::~PhysicsObjectSpawner()
 
 void PhysicsObjectSpawner::BeginGame()
 {
-    engine->GetInputManager()->AddKeyboardInputDelegate(KEY_MAP::SPACE, INPUT_ACTION::G_PRESS, [this]() { CreatePhysicsBox(); });
+    //engine->GetInputManager()->AddKeyboardInputDelegate(KEY_MAP::SPACE, INPUT_ACTION::G_PRESS, [this]() { CreatePhysicsBox(); });
+    engine->GetInputManager()->AddKeyboardInputDelegate(KEY_MAP::SPACE, INPUT_ACTION::G_PRESS, [this]() { ThrowPhysicsSphere(); });
+}
+
+void PhysicsObjectSpawner::Tick(float deltaTime)
+{
+    static float counter = 0.25f;
+
+    if(counter < 0.f)
+    {
+        counter += 0.25f;
+        CreatePhysicsBox();
+    }
+
+    counter -= deltaTime;
 }
 
 void PhysicsObjectSpawner::CreatePhysicsBox()
@@ -33,6 +52,19 @@ void PhysicsObjectSpawner::CreatePhysicsBox()
     float randomPositionY = positionDistY(rd);
 
     PhysicsBox* physicsBox = new PhysicsBox();
-    physicsBox->SetMass(100.f);
     physicsBox->SetWorldPosition(initialPosition_ + Vector3{ randomPositionX, randomPositionY, 10.f });
+}
+
+void PhysicsObjectSpawner::ThrowPhysicsSphere()
+{
+    PhysicsSphere* physicsSphere = new PhysicsSphere();
+    physicsSphere->SetWorldScaling(Vector3(0.2f));
+
+    Game* game = dynamic_cast<Game*>(engine->GetApplication());
+
+    Archer* archer = game->GetArcher();
+
+    physicsSphere->SetWorldPosition(archer->GetWorldPosition() + archer->GetUpVector() + archer->GetForwardVector() * 0.25f);
+    physicsSphere->SetLinearVelocity(50.f * (archer->GetForwardVector() * 0.85f + archer->GetUpVector() * 0.15f));
+    //physicsSphere->ApplyForce(1000000.f * (archer->GetForwardVector() * 0.9f + archer->GetUpVector() * 0.1f));
 }
