1b507570254f52fb30f69a91dcd6f7066626b66e
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2024-02-01 22:15:08 +0300

    Add GOKNAR_DEBUG and GOKNAR_RELEASE macros, and EngineContent folder

--- Goknar/Source/Goknar/Core.cpp
+++ Goknar/Source/Goknar/Core.cpp
@@ -5,3 +5,11 @@ const std::string ContentDir = CONTENT_DIR;
 #else
 const std::string ContentDir = "";
 #endif
+
+#ifdef GOKNAR_DEBUG
+    #ifdef ENGINE_CONTENT_DIR
+    const std::string EngineContentDir = ENGINE_CONTENT_DIR;
+    #else
+    const std::string EngineContentDir = "";
+    #endif
+#endif
