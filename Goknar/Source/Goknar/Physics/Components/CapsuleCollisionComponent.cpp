115c7aa77c88f8042af0219b9299a65ef358f7e1
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2024-02-13 23:49:06 +0300

    Make btCapsuleComponent creation as in bullet physics for now. Collision component's axis are not suitable for game world

--- Goknar/Source/Goknar/Physics/Components/CapsuleCollisionComponent.cpp
+++ Goknar/Source/Goknar/Physics/Components/CapsuleCollisionComponent.cpp
@@ -20,7 +20,7 @@ CapsuleCollisionComponent::~CapsuleCollisionComponent()
 
 void CapsuleCollisionComponent::PreInit()
 {
-	bulletCollisionShape_ = new btCapsuleShape(height_, radius_);
+	bulletCollisionShape_ = new btCapsuleShape(radius_, height_);
 
 	CollisionComponent::PreInit();
 }
