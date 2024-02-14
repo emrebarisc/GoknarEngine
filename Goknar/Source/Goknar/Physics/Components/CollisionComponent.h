f49442f6a2d1b4a191da1ab75948c6ad28048526
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2024-02-14 22:31:24 +0300

    Update CollisionComponent overlap function calls and do not call delegates if it's not active

--- Goknar/Source/Goknar/Physics/Components/CollisionComponent.h
+++ Goknar/Source/Goknar/Physics/Components/CollisionComponent.h
@@ -57,6 +57,39 @@ public:
 		collisionMask_ = collisionMask;
 	}
 
+	void OverlapBegin(PhysicsObject* otherObject, CollisionComponent* otherComponent, const Vector3& hitPosition, const Vector3& hitNormal)
+	{
+		// TODO: Find a better way for active controlling.
+		// Disabling bullet overlapping on bullet side is the best way but
+		// Not working with btGhostObject(it works fine with btRigidBody)
+		if(isActive_ && !OnOverlapBegin.isNull())
+		{
+			OnOverlapBegin(otherObject, otherComponent, hitPosition, hitNormal);
+		}
+	}
+
+	void OverlapContinue(PhysicsObject* otherObject, CollisionComponent* otherComponent, const Vector3& hitPosition, const Vector3& hitNormal)
+	{
+		// TODO: Find a better way for active controlling.
+		// Disabling bullet overlapping on bullet side is the best way but
+		// Not working with btGhostObject(it works fine with btRigidBody)
+		if(isActive_ && !OnOverlapContinue.isNull())
+		{
+			OnOverlapContinue(otherObject, otherComponent, hitPosition, hitNormal);
+		}
+	}
+
+	void OverlapEnd(PhysicsObject* otherObject, CollisionComponent* otherComponent)
+	{
+		// TODO: Find a better way for active controlling.
+		// Disabling bullet overlapping on bullet side is the best way but
+		// Not working with btGhostObject(it works fine with btRigidBody)
+		if(isActive_ && !OnOverlapEnd.isNull())
+		{
+			OnOverlapEnd(otherObject, otherComponent);
+		}
+	}	
+
 	Delegate<OverlapBeginAlias> OnOverlapBegin;
 	Delegate<OverlapContinueAlias> OnOverlapContinue;
 	Delegate<OverlapEndAlias> OnOverlapEnd;
