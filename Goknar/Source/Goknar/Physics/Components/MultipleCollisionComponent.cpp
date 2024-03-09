812aa43655c2c845d443c95877dd8613e9f768c2
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2024-03-09 17:55:46 +0300

    Add MultipleCollisionComponent

---
+++ Goknar/Source/Goknar/Physics/Components/MultipleCollisionComponent.cpp
@@ -0,0 +1,81 @@
+#include "pch.h"
+
+#include "BulletCollision/CollisionShapes/btCompoundShape.h"
+
+#include "MultipleCollisionComponent.h"
+#include "Log.h"
+#include "ObjectBase.h"
+#include "Physics/PhysicsUtils.h"
+	
+MultipleCollisionComponent::MultipleCollisionComponent(Component* parent) :
+	CollisionComponent(parent)
+{
+}
+
+MultipleCollisionComponent::MultipleCollisionComponent(ObjectBase* parentObjectBase) :
+	CollisionComponent(parentObjectBase)
+{
+}
+
+MultipleCollisionComponent::~MultipleCollisionComponent()
+{
+}
+
+void MultipleCollisionComponent::PreInit()
+{
+	bulletCompoundShape_ = new btCompoundShape();
+	bulletCollisionShape_ = bulletCompoundShape_;
+	
+	CollisionComponent::PreInit();
+}
+
+void MultipleCollisionComponent::Init()
+{
+	CollisionComponent::Init();
+}
+
+void MultipleCollisionComponent::PostInit()
+{
+	std::vector<CollisionComponent*>::const_iterator subCollisionComponentIterator = subCollisionComponents_->cbegin();
+
+	while(subCollisionComponentIterator != subCollisionComponents_->cend())
+	{
+		AddCollisionComponentInner(*subCollisionComponentIterator);
+
+		++subCollisionComponentIterator;
+	}
+
+	delete subCollisionComponents_;
+
+	CollisionComponent::PostInit();
+}
+	
+void MultipleCollisionComponent::BeginGame()
+{
+	CollisionComponent::BeginGame();
+}
+
+void MultipleCollisionComponent::TickComponent(float deltaTime)
+{
+	CollisionComponent::TickComponent(deltaTime);
+}
+
+void MultipleCollisionComponent::AddCollisionComponent(CollisionComponent* collisionComponent)
+{
+	if(!GetIsInitialized())
+	{
+		subCollisionComponents_->push_back(collisionComponent);
+		return;
+	}
+
+	AddCollisionComponentInner(collisionComponent);
+}
+
+void MultipleCollisionComponent::AddCollisionComponentInner(CollisionComponent* collisionComponent)
+{
+	btCollisionShape* bulletCollisionComponent = collisionComponent->GetBulletCollisionShape();
+
+	btTransform bulletTransform = PhysicsUtils::GetBulletTransform(collisionComponent->GetRelativeRotation(), collisionComponent->GetRelativePosition());
+
+	bulletCompoundShape_->addChildShape(bulletTransform, bulletCollisionComponent);
+}
---
+++ Goknar/Source/Goknar/Physics/Components/MultipleCollisionComponent.h
@@ -0,0 +1,40 @@
+#ifndef __MULTIPLECOLLISIONCOMPONENT_H__
+#define __MULTIPLECOLLISIONCOMPONENT_H__
+
+#include <vector>
+
+#include "CollisionComponent.h"
+
+class btCompoundShape;
+
+class GOKNAR_API MultipleCollisionComponent : public CollisionComponent
+{
+public:
+	MultipleCollisionComponent(Component* parent);
+	MultipleCollisionComponent(ObjectBase* parentObjectBase);
+	~MultipleCollisionComponent();
+
+	virtual void PreInit() override;
+	virtual void Init() override;
+	virtual void PostInit() override;
+	
+	virtual void BeginGame() override;
+	virtual void TickComponent(float deltaTime) override;
+
+	virtual void AddCollisionComponent(CollisionComponent* collisionComponent);
+
+	btCompoundShape* GetBulletCompoundShape() const
+	{
+		return bulletCompoundShape_;
+	}
+
+protected:
+private:
+	void AddCollisionComponentInner(CollisionComponent* collisionComponent);
+
+	btCompoundShape* bulletCompoundShape_;
+
+	std::vector<CollisionComponent*>* subCollisionComponents_{ new std::vector<CollisionComponent*>() };
+};
+
+#endif
