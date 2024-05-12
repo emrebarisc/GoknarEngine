e4ab829faf951e00e0a5def94f1cd2303c5c315e
emrebarisc <22220055+emrebarisc@users.noreply.github.com>
2024-05-12 16:47:36 +0300

    Upgrade DebugDrawer Add DrawCapsule and DrawSphere functionalities Bugfix

--- Goknar/Source/Goknar/Debug/DebugDrawer.cpp
+++ Goknar/Source/Goknar/Debug/DebugDrawer.cpp
@@ -59,7 +59,7 @@ void DebugDrawer::DrawCircle(const Vector3& position, const Quaternion& rotation
 	{
 		Vector3 start = Vector3{ radius * cosf(angle), radius * sinf(angle), 0.f };
 		Vector3 end = Vector3{ radius * cosf(angle + angleStep), radius * sinf(angle + angleStep), 0.f };
-		DrawLine(start, end, color, thickness, time, circle);
+		DrawLine(start, end, color, thickness, -1.f, circle);
 	}
 
 	circle->SetWorldPosition(position, false);
@@ -67,73 +67,249 @@ void DebugDrawer::DrawCircle(const Vector3& position, const Quaternion& rotation
 	circle->SetParent(owner);
 }
 
-void DebugDrawer::DrawHalfSphere(const Vector3& position, const Quaternion& rotation, float radius, const Colorf& color, float thickness, float time, ObjectBase* owner)
+void DebugDrawer::DrawSphere(const Vector3& position, const Quaternion& rotation, float radius, const Colorf& color, float thickness, float time, ObjectBase* owner)
 {
-	ObjectBase* halfSphere = new ObjectBase();
+	ObjectBase* sphere = new ObjectBase();
 
-	halfSphere->SetWorldPosition(position, false);
-	halfSphere->SetWorldRotation(rotation, false);
-	halfSphere->SetParent(owner);
-}
+	Matrix rotationMatrix = rotation.GetMatrix();
+	Vector3 up = rotationMatrix.GetUpVector();
+	Vector3 axis = rotationMatrix.GetForwardVector();
+	float minTh = -SIMD_HALF_PI;
+	float maxTh = SIMD_HALF_PI;
+	float minPs = -SIMD_HALF_PI;
+	float maxPs = SIMD_HALF_PI;
+	float stepDegrees = 30.f;
 
-void DebugDrawer::DrawSphere(const Vector3& position, float radius, const Colorf& color, float thickness, float time, ObjectBase* owner)
-{
+	DrawSpherePatch(position, up, axis, radius, minTh, maxTh, minPs, maxPs, color, stepDegrees, false, thickness, -1.f, sphere);
+	DrawSpherePatch(position, up, -axis, radius, minTh, maxTh, minPs, maxPs, color, stepDegrees, false, thickness, -1.f, sphere);
+
+	sphere->SetName("DebugSphere");
+	sphere->SetParent(owner, SnappingRule::None);
 }
 
 void DebugDrawer::DrawBox(const Vector3& position, const Quaternion& rotation, const Vector3& halfSize, const Colorf& color, float thickness, float time, ObjectBase* owner)
 {
 	ObjectBase* box = new ObjectBase();
 
-	Vector4 rotatedHalfSize = rotation.GetMatrix() * Vector4{ halfSize, 1.f };
+	Vector4 rotatedHalfSize = rotation.GetMatrix() * Vector4{ halfSize, 0.f };
+
+	Matrix rotationMatrix = rotation.GetMatrix();
+	Vector3 forwardVector = halfSize * rotationMatrix.GetForwardVector();
+	Vector3 leftVector = halfSize * rotationMatrix.GetLeftVector();
+	Vector3 upVector = halfSize * rotationMatrix.GetUpVector();
 
 	Vector3 corners[8] =
 	{
-		{ position.x - rotatedHalfSize.x, position.y - rotatedHalfSize.y, position.z - rotatedHalfSize.z },
-		{ position.x + rotatedHalfSize.x, position.y - rotatedHalfSize.y, position.z - rotatedHalfSize.z },
-		{ position.x - rotatedHalfSize.x, position.y + rotatedHalfSize.y, position.z - rotatedHalfSize.z },
-		{ position.x + rotatedHalfSize.x, position.y + rotatedHalfSize.y, position.z - rotatedHalfSize.z },
-		{ position.x - rotatedHalfSize.x, position.y - rotatedHalfSize.y, position.z + rotatedHalfSize.z },
-		{ position.x + rotatedHalfSize.x, position.y - rotatedHalfSize.y, position.z + rotatedHalfSize.z },
-		{ position.x - rotatedHalfSize.x, position.y + rotatedHalfSize.y, position.z + rotatedHalfSize.z },
-		{ position.x + rotatedHalfSize.x, position.y + rotatedHalfSize.y, position.z + rotatedHalfSize.z } 
+		{ position - forwardVector - leftVector - upVector },
+		{ position + forwardVector - leftVector - upVector },
+		{ position - forwardVector + leftVector - upVector },
+		{ position + forwardVector + leftVector - upVector },
+		{ position - forwardVector - leftVector + upVector },
+		{ position + forwardVector - leftVector + upVector },
+		{ position - forwardVector + leftVector + upVector },
+		{ position + forwardVector + leftVector + upVector } 
 	};
 
-	DrawLine(corners[0], corners[1], color, thickness, time, box);
-	DrawLine(corners[0], corners[2], color, thickness, time, box);
+	DrawLine(corners[0], corners[1], color, thickness, -1.f, box);
+	DrawLine(corners[0], corners[2], color, thickness, -1.f, box);
 
-	DrawLine(corners[3], corners[1], color, thickness, time, box);
-	DrawLine(corners[3], corners[2], color, thickness, time, box);
+	DrawLine(corners[3], corners[1], color, thickness, -1.f, box);
+	DrawLine(corners[3], corners[2], color, thickness, -1.f, box);
 
-	DrawLine(corners[4], corners[5], color, thickness, time, box);
-	DrawLine(corners[4], corners[6], color, thickness, time, box);
+	DrawLine(corners[4], corners[5], color, thickness, -1.f, box);
+	DrawLine(corners[4], corners[6], color, thickness, -1.f, box);
 
-	DrawLine(corners[7], corners[5], color, thickness, time, box);
-	DrawLine(corners[7], corners[6], color, thickness, time, box);
+	DrawLine(corners[7], corners[5], color, thickness, -1.f, box);
+	DrawLine(corners[7], corners[6], color, thickness, -1.f, box);
 
-	DrawLine(corners[0], corners[4], color, thickness, time, box);
-	DrawLine(corners[1], corners[5], color, thickness, time, box);
-	DrawLine(corners[2], corners[6], color, thickness, time, box);
-	DrawLine(corners[3], corners[7], color, thickness, time, box);
+	DrawLine(corners[0], corners[4], color, thickness, -1.f, box);
+	DrawLine(corners[1], corners[5], color, thickness, -1.f, box);
+	DrawLine(corners[2], corners[6], color, thickness, -1.f, box);
+	DrawLine(corners[3], corners[7], color, thickness, -1.f, box);
 
 	box->SetName("DebugBox");
-	box->SetParent(owner);
+	box->SetParent(owner, SnappingRule::None);
 }
 
-void DebugDrawer::DrawCapsule(const Vector3& position, float radius, const Colorf& color, float thickness, float time, ObjectBase* owner)
+void DebugDrawer::DrawCapsule(	const Vector3& position, const Quaternion& rotation, 
+								float radius, float height, const Colorf& color, 
+								float thickness/* = 1.f*/, float time/* = -1.f*/, ObjectBase* owner/* = nullptr*/)
 {
+	ObjectBase* capsule = new ObjectBase();
+
+	int stepDegrees = 30;
+
+	float halfHeight = height * 0.5f;
+
+	Vector3 capStart(0.f, 0.f, 0.f);
+	capStart.z = -halfHeight;
+
+	Vector3 capEnd(0.f, 0.f, 0.f);
+	capEnd.z = halfHeight;
+
+	Matrix rotationMatrix = rotation.GetMatrix();
+
+	{
+		Vector3 center = position + capStart;
+		Vector3 up = rotationMatrix.GetForwardVector();
+		Vector3 forward = -rotationMatrix.GetUpVector();
+		float minTh = -HALF_PI;
+		float maxTh = HALF_PI;
+		float minPs = -HALF_PI;
+		float maxPs = HALF_PI;
+
+		DrawSpherePatch(center, up, forward, radius, minTh, maxTh, minPs, maxPs, color, stepDegrees, false, thickness, -1.f, capsule);
+	}
+
+	{
+		Vector3 center = position + capEnd;
+		Vector3 up = rotationMatrix.GetForwardVector();
+		Vector3 forward = rotationMatrix.GetUpVector();
+		float minTh = -HALF_PI;
+		float maxTh = HALF_PI;
+		float minPs = -HALF_PI;
+		float maxPs = HALF_PI;
+		DrawSpherePatch(center, up, forward, radius, minTh, maxTh, minPs, maxPs, color, stepDegrees, false, thickness, -1.f, capsule);
+	}
+
+	for (int i = 0; i < 360; i += stepDegrees)
+	{
+		capEnd.x = capStart.x = GoknarMath::Sin(i * TO_RADIAN) * radius;
+		capEnd.y = capStart.y = GoknarMath::Cos(i * TO_RADIAN) * radius;
+		DrawLine(
+			position + rotationMatrix * Vector4{ capStart, 1.f }, 
+			position + rotationMatrix * Vector4{ capEnd, 1.f }, 
+			color, thickness, -1.f, capsule);
+	}
+
+	capsule->SetName("DebugCapsule");
+	capsule->SetParent(owner, SnappingRule::None);
 }
 
-void DebugDrawer::DrawCollisionComponent(BoxCollisionComponent* boxCollisionComponent, const Colorf& color, float thickness, float time, ObjectBase* owner)
+void DebugDrawer::DrawCollisionComponent(BoxCollisionComponent* boxCollisionComponent, const Colorf& color, float thickness, float time)
 {
-	DrawBox(boxCollisionComponent->GetWorldPosition(), boxCollisionComponent->GetWorldRotation(), boxCollisionComponent->GetHalfSize(), color, thickness, time, owner);
+	DrawBox(Vector3::ZeroVector, Quaternion::Identity, boxCollisionComponent->GetHalfSize(), color, thickness, time, boxCollisionComponent->GetOwner());
 }
 
-void DebugDrawer::DrawCollisionComponent(CapsuleCollisionComponent* capsuleCollisionComponent, const Colorf& color, float thickness, float time, ObjectBase* owner)
+void DebugDrawer::DrawCollisionComponent(CapsuleCollisionComponent* capsuleCollisionComponent, const Colorf& color, float thickness, float time)
 {
-	DrawCapsule(capsuleCollisionComponent->GetWorldPosition(), capsuleCollisionComponent->GetRadius(), color, thickness, time, owner);
+	DrawCapsule(
+		Vector3::ZeroVector, Quaternion::Identity, 
+		capsuleCollisionComponent->GetRadius(), capsuleCollisionComponent->GetHeight(), 
+		color, thickness, time, capsuleCollisionComponent->GetOwner());
 }
 
-void DebugDrawer::DrawCollisionComponent(SphereCollisionComponent* sphereCollisionComponent, const Colorf& color, float thickness, float time, ObjectBase* owner)
+void DebugDrawer::DrawCollisionComponent(SphereCollisionComponent* sphereCollisionComponent, const Colorf& color, float thickness, float time)
 {
-	DrawSphere(sphereCollisionComponent->GetWorldPosition(), sphereCollisionComponent->GetRadius(), color, thickness, time, owner);
+	DrawSphere(Vector3::ZeroVector, Quaternion::Identity, sphereCollisionComponent->GetRadius(), color, thickness, time, sphereCollisionComponent->GetOwner());
+}
+
+void DebugDrawer::DrawSpherePatch(const Vector3& center, const Vector3& up, const Vector3& forward, float radius,
+								 float minTh, float maxTh, float minPs, float maxPs, const Colorf& color, 
+								 float stepDegrees/* = 10.f*/, bool drawCenter/* = true*/, 
+								 float thickness/* = 1.f*/, float time/* = -1.f*/, ObjectBase* owner/* = nullptr*/)
+{
+	Vector3 vA[74];
+	Vector3 vB[74];
+	Vector3 *pvA = vA, *pvB = vB, *pT;
+	Vector3 npole = center + up * radius;
+	Vector3 spole = center - up * radius;
+	Vector3 arcStart;
+	float step = stepDegrees * TO_RADIAN;
+	const Vector3& kv = up;
+	const Vector3& iv = forward;
+	Vector3 jv = kv.Cross(iv);
+	bool drawN = false;
+	bool drawS = false;
+	if (minTh <= -HALF_PI)
+	{
+		minTh = -HALF_PI + step;
+		drawN = true;
+	}
+	if (maxTh >= HALF_PI)
+	{
+		maxTh = HALF_PI - step;
+		drawS = true;
+	}
+	if (minTh > maxTh)
+	{
+		minTh = -HALF_PI + step;
+		maxTh = HALF_PI - step;
+		drawN = drawS = true;
+	}
+	int n_hor = (int)((maxTh - minTh) / step) + 1;
+	if (n_hor < 2) n_hor = 2;
+	float step_h = (maxTh - minTh) / float(n_hor - 1);
+	bool isClosed = false;
+	if (minPs > maxPs)
+	{
+		minPs = -SIMD_PI + step;
+		maxPs = SIMD_PI;
+		isClosed = true;
+	}
+	else if ((maxPs - minPs) >= SIMD_PI * float(2.f))
+	{
+		isClosed = true;
+	}
+	else
+	{
+		isClosed = false;
+	}
+	int n_vert = (int)((maxPs - minPs) / step) + 1;
+	if (n_vert < 2) n_vert = 2;
+	float step_v = (maxPs - minPs) / float(n_vert - 1);
+	for (int i = 0; i < n_hor; i++)
+	{
+		float th = minTh + i * step_h;
+		float sth = radius * GoknarMath::Sin(th);
+		float cth = radius * GoknarMath::Cos(th);
+		for (int j = 0; j < n_vert; j++)
+		{
+			float psi = minPs + float(j) * step_v;
+			float sps = GoknarMath::Sin(psi);
+			float cps = GoknarMath::Cos(psi);
+			pvB[j] = center + cth * cps * iv + cth * sps * jv + sth * kv;
+			if (i)
+			{
+				DrawLine(pvA[j], pvB[j], color, thickness, -1.f, owner);
+			}
+			else if (drawS)
+			{
+				DrawLine(spole, pvB[j], color, thickness, -1.f, owner);
+			}
+			if (j)
+			{
+				DrawLine(pvB[j - 1], pvB[j], color, thickness, -1.f, owner);
+			}
+			else
+			{
+				arcStart = pvB[j];
+			}
+			if ((i == (n_hor - 1)) && drawN)
+			{
+				DrawLine(npole, pvB[j], color, thickness, -1.f, owner);
+			}
+
+			if (drawCenter)
+			{
+				if (isClosed)
+				{
+					if (j == (n_vert - 1))
+					{
+						DrawLine(arcStart, pvB[j], color, thickness, -1.f, owner);
+					}
+				}
+				else
+				{
+					if (((!i) || (i == (n_hor - 1))) && ((!j) || (j == (n_vert - 1))))
+					{
+						DrawLine(center, pvB[j], color, thickness, -1.f, owner);
+					}
+				}
+			}
+		}
+		pT = pvA;
+		pvA = pvB;
+		pvB = pT;
+	}
 }
--- Goknar/Source/Goknar/Debug/DebugDrawer.h
+++ Goknar/Source/Goknar/Debug/DebugDrawer.h
@@ -18,16 +18,21 @@ public:
 	DebugDrawer();
 	~DebugDrawer();
 
-	static void DrawLine(const Vector3& start, const Vector3& end, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
-	static void DrawBox(const Vector3& position, const Quaternion& rotation, const Vector3& halfSize, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
-	static void DrawCapsule(const Vector3& position, float radius, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
-	static void DrawCircle(const Vector3& position, const Quaternion& rotation, float radius, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
-	static void DrawHalfSphere(const Vector3& position, const Quaternion& rotation, float radius, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
-	static void DrawSphere(const Vector3& position, float radius, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
+	static void DrawLine(const Vector3& start, const Vector3& end, const Colorf& color, float thickness = 1.f, float time = -1.f, ObjectBase* owner = nullptr);
+	static void DrawBox(const Vector3& position, const Quaternion& rotation, const Vector3& halfSize, const Colorf& color, float thickness = 1.f, float time = -1.f, ObjectBase* owner = nullptr);
+	static void DrawCapsule(const Vector3& position, const Quaternion& rotation, 
+								float radius, float height, const Colorf& color, 
+								float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
+	static void DrawCircle(const Vector3& position, const Quaternion& rotation, float radius, const Colorf& color, float thickness = 1.f, float time = -1.f, ObjectBase* owner = nullptr);
+	static void DrawSphere(const Vector3& position, const Quaternion& rotation, float radius, const Colorf& color, float thickness = 1.f, float time = -1.f, ObjectBase* owner = nullptr);
+	static void DrawSpherePatch(const Vector3& center, const Vector3& up, const Vector3& axis, float radius,
+								 float minTh, float maxTh, float minPs, float maxPs, const Colorf& color, 
+								 float stepDegrees = float(10.f), bool drawCenter = true, 
+								 float thickness = 1.f, float time = -1.f, ObjectBase* owner = nullptr);
 
-	static void DrawCollisionComponent(BoxCollisionComponent* boxCollisionComponent, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
-	static void DrawCollisionComponent(CapsuleCollisionComponent* capsuleCollisionComponent, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
-	static void DrawCollisionComponent(SphereCollisionComponent* sphereCollisionComponent, const Colorf& color, float thickness = 1.f, float time = 1.f, ObjectBase* owner = nullptr);
+	static void DrawCollisionComponent(BoxCollisionComponent* boxCollisionComponent, const Colorf& color, float thickness = 1.f, float time = -1.f);
+	static void DrawCollisionComponent(CapsuleCollisionComponent* capsuleCollisionComponent, const Colorf& color, float thickness = 1.f, float time = -1.f);
+	static void DrawCollisionComponent(SphereCollisionComponent* sphereCollisionComponent, const Colorf& color, float thickness = 1.f, float time = -1.f);
 
 protected:
 
