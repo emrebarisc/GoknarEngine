a3427a36d49da5199147531fa30c7d5e42f27cb6
Emre Barış Coşkun <22220055+emrebarisc@users.noreply.github.com>
2024-09-02 23:21:10 +0300

    Update directional light bias constant

--- Goknar/Source/Goknar/Lights/DirectionalLight.cpp
+++ Goknar/Source/Goknar/Lights/DirectionalLight.cpp
@@ -61,7 +61,7 @@ void DirectionalLight::PreInit()
 		{
 			0.5f, 0.f, 0.f, 0.5f,
 			0.f, 0.5f, 0.f, 0.5f,
-			0.f, 0.f, 0.5f, 0.5f - 0.001f,
+			0.f, 0.f, 0.5f, 0.5f - 0.00001f,
 			0.f, 0.f, 0.f, 1.f
 		};
 		UpdateBiasedShadowMatrix();
